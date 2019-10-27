5fd1a5a0b446eeeca7ddeaf9fe7c287056e23032
digruttolagabriel <digruttolagabriel@gmail.com>
2019-10-26 23:28:18 -0300

    first commit

---
+++ src/main/java/com/gabrielDigruttola/APIFacturacion/ApiFacturacionApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielDigruttola.APIFacturacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFacturacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFacturacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielDigruttola/APIFacturacion/ApiFacturacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielDigruttola.APIFacturacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFacturacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
